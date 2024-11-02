e218d05b97476461e30439371686f49f02cdafca
kmtusher97 <kmtusher97@gmail.com>
2024-11-02 22:08:22 +0600

    Add models

---
+++ backend/src/models/file.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const FileSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const File = mongoose.model('File', FileSchema);
