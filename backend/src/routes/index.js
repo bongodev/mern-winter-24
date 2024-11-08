e1f604f5486e1c7628424c4a853c266365d142a4
kmtusher97 <kmtusher97@gmail.com>
2024-11-08 21:58:39 +0600

    Add authentication middleware

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
+import { authMiddleware } from '../middlewares/index.js';
+
 import { authRouter } from './auth.js';
 import { productRouter } from './products.js';
 import { userRouter } from './users.js';
 
 export const configureRoutes = (app) => {
   app.use('/api/auth', authRouter);
-  app.use('/api', productRouter);
+  app.use('/api', authMiddleware, productRouter);
   app.use('/api/users', userRouter);
 };
