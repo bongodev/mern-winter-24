e57d02a037a5c319355f966d6b37119fee72eb3d
kmtusher97 <kmtusher97@gmail.com>
2024-11-08 21:51:30 +0600

    Move mock product API's to product module

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,6 @@ import { userRouter } from './users.js';
 
 export const configureRoutes = (app) => {
   app.use('/api/auth', authRouter);
-  app.use('/api/v2/products', productRouter);
+  app.use('/api', productRouter);
   app.use('/api/users', userRouter);
 };
